506d56b106445c1e05f6274f2c652c44e66aa463
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2025-04-12 21:40:52 +0200

    Fixed Flask structure

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,3 @@ from flask import Flask, url_for
 app = Flask(__name__)
 
 from app import routes
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -17,39 +17,3 @@ def ksiega2():
 @app.route("/ksiega3")
 def ksiega3():
     return render_template("ksiega3.html")
-
-@app.route("/ksiega4")
-def ksiega4():
-    return render_template("ksiega4.html")
-
-@app.route("/ksiega5")
-def ksiega5():
-    return render_template("ksiega5.html")
-
-@app.route("/ksiega6")
-def ksiega6():
-    return render_template("ksiega6.html")
-
-@app.route("/ksiega7")
-def ksiega7():
-    return render_template("ksiega7.html")
-
-@app.route("/ksiega8")
-def ksiega8():
-    return render_template("ksiega8.html")
-
-@app.route("/ksiega9")
-def ksiega9():
-    return render_template("ksiega9.html")
-
-@app.route("/ksiega10")
-def ksiega10():
-    return render_template("ksiega10.html")
-
-@app.route("/ksiega11")
-def ksiega11():
-    return render_template("ksiega11.html")
-
-@app.route("/ksiega12")
-def ksiega12():
-    return render_template("ksiega12.html")
